fb004f55ca4f11db17dc2d16880bc71753a677dc
Tobin Fricke <tobin@pas.rochester.edu>
2012-10-03 21:15:24 +0200

    don't remember

--- 2011/2.c
+++ 2011/2.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <assert.h>
 
-int count_lucky_tickets(int digits, int base) {
-  assert((2 <= base) && (base <= 10));
-  assert(digits % 2 == 0);
+int *counts = 0;
 
-  
+int visit(int n, int b) {
+  int sum = 0;
+  if (n<0) return 0;
+  if (n==0) { // base case
+    int ii=0;
+    while (counts[ii] >= 0) {
+      sum += counts[ii];
+      ii++;
+    }
+    return sum;
+  }  
+  for (int ii=0; ii<b; ii++) 
+    sum += visit(n-b, b-1);
+  return sum;
+}
+
+// n = number of digits
+// b = base
+int count_lucky_tickets(int n, int b) {
+  assert((2 <= b) && (b <= 10));
+  assert(n % 2 == 0);
+
+  counts = (int *)calloc(n, sizeof(int));
+  counts[n] = -1;
+
+  int result = visit(n, b);
+  free(counts);
+
   // "The answers will always be less than 2^(63)"
-
+  return result;
 }
 
 int main(int argc, char **argv) {
+  int digits, base;
   while (2 == scanf("%d %d", &digits, &base)) {
     int result = count_lucky_tickets(digits, base);
     printf("There are %d %d-digit base %d lucky numbers.\n",
